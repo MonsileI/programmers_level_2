93103afc62c16e60ecc5c4f4389b96d40d69cce3
monsileI <https://github.com/MonsileI/programmers_level_2.git>
2022-02-16 08:01:46 +0900

    contribution

--- programmers_level_2/src/first_22_02_15/test.java
+++ programmers_level_2/src/first_22_02_15/test.java
@@ -7,19 +7,6 @@ public class test {
 	
 	public static void main(String[] args) {
 		
-	PriorityQueue<Integer> heap = new PriorityQueue<>();
-	
-	heap.offer(4);
-	heap.offer(2);
-	heap.offer(3);
-	heap.offer(4);
-	heap.offer(5);
-	
-	System.out.println(heap.poll());
-	System.out.println(heap.poll());
-	System.out.println(heap.poll());
-	System.out.println(heap.poll());
-	System.out.println(heap.poll());
 	
 	
 	}

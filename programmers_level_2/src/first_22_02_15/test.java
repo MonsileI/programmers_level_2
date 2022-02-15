7647eadfc576e9944e3911239222a86c617ca8bb
monsileI <hansaem900d@kakao.com>
2022-02-16 08:15:53 +0900

    delete Test

--- programmers_level_2/src/first_22_02_15/test.java
+++ programmers_level_2/src/first_22_02_15/test.java
@@ -1,14 +1,9 @@
 package first_22_02_15;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class test {
-	
+
 	public static void main(String[] args) {
 		
-	
-	
+		System.out.println("외않되!");
 	}
-	
 }

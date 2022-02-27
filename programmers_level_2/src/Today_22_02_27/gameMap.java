d8b7998e1b6754348a1e926ecaa5266f796ce64a
monsileI <hansaem900d@kakao.com>
2022-02-27 19:49:44 +0900

    결국 지우고 지우고 지우고.. 꼭 내가 풀고 만다

---
+++ programmers_level_2/src/Today_22_02_27/gameMap.java
@@ -0,0 +1,19 @@
+package Today_22_02_27;
+
+public class gameMap {
+	
+	public static void main(String[] args) {
+		
+		int[][]maps = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}};
+		
+		int answer = 1;
+		
+		int n = maps.length;
+		int m = maps[0].length;
+		//어떠케 하는거야ㅠㅠㅠㅠ
+		
+		
+	
+	}
+
+}

aed902b87130d35cdbdf39ab2cc941840e8279e9
monsileI <hansaem900d@kakao.com>
2022-03-04 21:28:09 +0900

    진짜 어렵네 ㅠㅠ

---
+++ programmers_level_2/src/Today_22_03_02/dd.java
@@ -0,0 +1,13 @@
+package Today_22_03_02;
+
+public class dd {
+
+	public static void main(String[] args) {
+		
+		String[] test = {"a","b","c"};
+		
+		
+		
+		
+	}
+}
--- programmers_level_2/src/Today_22_03_02/ststst.java
+++ programmers_level_2/src/Today_22_03_02/ststst.java
@@ -1,13 +1,22 @@
 package Today_22_03_02;
 
+import java.util.StringTokenizer;
+
 public class ststst {
 
+	static int answer = 0;
+	
 	public static void main(String[] args) {
 		
-		String s = "I am sam";
-		String comp = "am";
+		int n = 2; // 조건 개수
+		String[] data = {"N~F=0", "R~T>2"}; //조건
 		
-		System.out.println(s.contains(comp));
+		String[]figures = {"A", "C", "F", "J", "M", "N", "R", "T"}; //사람들
 		
+		
+		
+		System.out.println(answer);
 	}
+	
+	
 }
